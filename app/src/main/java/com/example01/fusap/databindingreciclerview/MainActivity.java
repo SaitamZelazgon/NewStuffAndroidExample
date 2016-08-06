3e5265a64868add4c7b587c6f478a46fd5f3577d
jorge daniel gonzalez <jorge daniel gonzalez>
2016-08-06 12:10:33 -0300

    JG:Bug columna Name Unique championDao

--- app/src/main/java/com/example01/fusap/databindingreciclerview/MainActivity.java
+++ app/src/main/java/com/example01/fusap/databindingreciclerview/MainActivity.java
@@ -71,90 +71,90 @@ public class MainActivity extends AppCompatActivity {
         EventBus.getDefault().register(mAdapter);
 
         // Request a string response from the provided URL.
+            if(ConnectionSingleton.getSession().getChampionDao().count() == 0) {
+                JSONObject response = new JSONObject();
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                        "https://global.api.pvp.net/api/lol/static-data/las/v1.2/champion?locale=en_US&champData=image,lore,stats&api_key=bdab9731-453d-400b-9b78-1ed18b613db5",
+                        response,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                new AsyncTask<JSONObject, Void, Void>() {
+                                    @Override
+                                    protected Void doInBackground(JSONObject... params) {
+                                        JSONObject response = params[0];
+                                        try {
+                                            Iterator<String> it = response.getJSONObject("data").keys();
 
-            JSONObject response = new JSONObject();
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
-                    "https://global.api.pvp.net/api/lol/static-data/las/v1.2/champion?locale=en_US&champData=image,lore,stats&api_key=bdab9731-453d-400b-9b78-1ed18b613db5",
-                    response,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            new AsyncTask<JSONObject, Void, Void>() {
-                                @Override
-                                protected Void doInBackground(JSONObject... params) {
-                                    JSONObject response = params[0];
-                                    try {
-                                        Iterator<String> it = response.getJSONObject("data").keys();
+                                            for (; it.hasNext(); ) {
+                                                String championName = it.next();
+                                                String imageUrl = "http://ddragon.leagueoflegends.com/cdn/5.14.1/img/champion/" + response.getJSONObject("data").getJSONObject(championName).getJSONObject("image").getString("full");
+                                                String lore = response.getJSONObject("data").getJSONObject(championName).getString("lore");
+                                                Long id = response.getJSONObject("data").getJSONObject(championName).getLong("id");
 
-                                        for (; it.hasNext(); ) {
-                                            String championName = it.next();
-                                            String imageUrl = "http://ddragon.leagueoflegends.com/cdn/5.14.1/img/champion/" + response.getJSONObject("data").getJSONObject(championName).getJSONObject("image").getString("full");
-                                            String lore = response.getJSONObject("data").getJSONObject(championName).getString("lore");
-                                            Long id = response.getJSONObject("data").getJSONObject(championName).getLong("id");
+                                                Champion champion = new Champion();
+                                                champion.setLore(lore);
+                                                champion.setImageUrl(imageUrl);
+                                                champion.setName(championName);
+                                                champion.setRiotApiId(id);
 
-                                            Champion champion = new Champion();
-                                            champion.setLore(lore);
-                                            champion.setImageUrl(imageUrl);
-                                            champion.setName(championName);
-                                            champion.setRiotApiId(id);
+                                                ChampionStats stats = new ChampionStats();
+                                                stats.setArmor(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("armor"));
+                                                stats.setArmorperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("armorperlevel"));
+                                                stats.setAttackdamage(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("attackdamage"));
+                                                stats.setAttackdamageperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("attackdamageperlevel"));
+                                                stats.setAttackrange(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("attackrange"));
+                                                stats.setAttackspeedoffset(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("attackspeedoffset"));
+                                                stats.setAttackspeedperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("attackspeedperlevel"));
+                                                stats.setCrit(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("crit"));
+                                                stats.setCritperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("critperlevel"));
+                                                stats.setHp(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("hp"));
+                                                stats.setHpperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("hpperlevel"));
+                                                stats.setHpregen(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("hpregen"));
+                                                stats.setHpregenperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("hpregenperlevel"));
+                                                stats.setMovespeed(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("movespeed"));
+                                                stats.setMp(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("mp"));
+                                                stats.setMpperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("mpperlevel"));
+                                                stats.setMpregen(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("mpregen"));
+                                                stats.setMpregenperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("mpregenperlevel"));
+                                                stats.setSpellblock(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("spellblock"));
+                                                stats.setSpellblockperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("spellblockperlevel"));
 
-                                            ChampionStats stats = new ChampionStats();
-                                            stats.setArmor(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("armor"));
-                                            stats.setArmorperlevel( response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("armorperlevel"));
-                                            stats.setAttackdamage(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("attackdamage"));
-                                            stats.setAttackdamageperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("attackdamageperlevel"));
-                                            stats.setAttackrange(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("attackrange"));
-                                            stats.setAttackspeedoffset(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("attackspeedoffset"));
-                                            stats.setAttackspeedperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("attackspeedperlevel"));
-                                            stats.setCrit(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("crit"));
-                                            stats.setCritperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("critperlevel"));
-                                            stats.setHp(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("hp"));
-                                            stats.setHpperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("hpperlevel"));
-                                            stats.setHpregen(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("hpregen"));
-                                            stats.setHpregenperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("hpregenperlevel"));
-                                            stats.setMovespeed(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("movespeed"));
-                                            stats.setMp(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("mp"));
-                                            stats.setMpperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("mpperlevel"));
-                                            stats.setMpregen(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("mpregen"));
-                                            stats.setMpregenperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("mpregenperlevel"));
-                                            stats.setSpellblock(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("spellblock"));
-                                            stats.setSpellblockperlevel(response.getJSONObject("data").getJSONObject(championName).getJSONObject("stats").getDouble("spellblockperlevel"));
+                                                ConnectionSingleton.getSession().getChampionStatsDao().insert(stats);
 
-                                            ConnectionSingleton.getSession().getChampionStatsDao().insert(stats);
+                                                champion.setAtributes(stats);
 
-                                            champion.setAtributes(stats);
-
-                                            ConnectionSingleton.getSession().getChampionDao().insert(champion);
+                                                ConnectionSingleton.getSession().getChampionDao().insert(champion);
+                                            }
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
                                         }
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
+                                        return null;
                                     }
-                                    return null;
-                                }
 
-                                @Override
-                                protected void onPostExecute(Void v) {
-                                    EventBus.getDefault().postSticky(new DataArrivedEvent());
-                                }
-                            }.execute(response);
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            EventBus.getDefault().postSticky(new MessageEvent("Connection Error."));
-                        }
-                    }) {
-            };
+                                    @Override
+                                    protected void onPostExecute(Void v) {
+                                        EventBus.getDefault().postSticky(new DataArrivedEvent());
+                                    }
+                                }.execute(response);
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                EventBus.getDefault().postSticky(new MessageEvent("Connection Error."));
+                            }
+                        }) {
+                };
 
-            // Add the request to the RequestQueue.
-            jsonObjectRequest.setTag(TAG);
-            jsonObjectRequest.setShouldCache(true);
-            jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(5000,
-                    15,
-                    2f));
-            NetworkCacheSingleton.getQueue().add(jsonObjectRequest);
-
+                // Add the request to the RequestQueue.
+                jsonObjectRequest.setTag(TAG);
+                jsonObjectRequest.setShouldCache(true);
+                jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(5000,
+                        15,
+                        2f));
+                NetworkCacheSingleton.getQueue().add(jsonObjectRequest);
+            }
     }
 
     @Override
