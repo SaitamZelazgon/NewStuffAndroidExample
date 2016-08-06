db1ad29c01eb75ec4ba534399a9d8b7a39e67c87
jorge daniel gonzalez <jorge daniel gonzalez>
2016-08-06 11:51:02 -0300

    JG:Mostrando stats activity transparente

--- app/src/main/java/com/example01/fusap/databindingreciclerview/LoreActivity.java
+++ app/src/main/java/com/example01/fusap/databindingreciclerview/LoreActivity.java
@@ -31,7 +31,26 @@ public class LoreActivity extends Activity {
     @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
     public void onChampionEvent(TranfersChampionEvent event){
         TextView stats = (TextView) findViewById(R.id.text_stats);
-        stats.setText("Armor:" + event.c.getAtributes().getArmor());
+        stats.setText("Armor:" + event.c.getAtributes().getArmor()+ "\n" +
+                      "armorperlevel:" + event.c.getAtributes().getArmorperlevel() + "\n" +
+                        "attackdamage:" + event.c.getAtributes().getAttackdamage() + "\n" +
+                        "attackdamageperlevel:" + event.c.getAtributes().getAttackdamageperlevel() + "\n" +
+                        "attackrange:" + event.c.getAtributes().getAttackrange() + "\n" +
+                        "attackspeedoffset:" + event.c.getAtributes().getAttackspeedoffset() + "\n" +
+                        "attackspeedperlevel:" + event.c.getAtributes().getAttackspeedperlevel() + "\n" +
+                        "crit:" + event.c.getAtributes().getCrit() + "\n" +
+                        "critperlevel:" + event.c.getAtributes().getCritperlevel() + "\n" +
+                        "hp:" + event.c.getAtributes().getHp() + "\n" +
+                        "hpperlevel:" + event.c.getAtributes().getHpperlevel() + "\n" +
+                        "hpregen:" + event.c.getAtributes().getHpregen() + "\n" +
+                        "hpregenperlevel:" + event.c.getAtributes().getHpregenperlevel() + "\n" +
+                        "movespeed:" + event.c.getAtributes().getMovespeed() + "\n" +
+                        "mp:" + event.c.getAtributes().getMp() + "\n" +
+                        "mpperlevel:" + event.c.getAtributes().getMpperlevel() + "\n" +
+                        "mpregen:" + event.c.getAtributes().getMpregen() + "\n" +
+                        "mpregenperlevel:" + event.c.getAtributes().getMpregenperlevel() + "\n" +
+                        "spellblock:" + event.c.getAtributes().getSpellblock() + "\n" +
+                        "spellblockperlevel:" + event.c.getAtributes().getSpellblockperlevel());
 
     }
 
