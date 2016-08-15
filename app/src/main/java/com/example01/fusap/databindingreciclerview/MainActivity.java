d3af951fa5e0e5fb6d5d0775254fbb410746437d
jorge daniel gonzalez <jorge daniel gonzalez>
2016-08-15 15:27:01 -0300

    JG: manejo mapa y marcadores

--- app/src/main/java/com/example01/fusap/databindingreciclerview/MainActivity.java
+++ app/src/main/java/com/example01/fusap/databindingreciclerview/MainActivity.java
@@ -1,22 +1,50 @@
 package com.example01.fusap.databindingreciclerview;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, ChampionListFragment.OnFragmentInteractionListener, OnMapReadyCallback {
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, ChampionListFragment.OnFragmentInteractionListener, OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, GoogleMap.OnMapClickListener,GoogleMap.OnMarkerClickListener, GoogleMap.OnMapLongClickListener {
 
     SupportMapFragment mapFragment;
+    GoogleMap map;
+    LocationManager locationManager;
+    Criteria criteria;
+    MarkerOptions markerOptions;
+    GoogleApiClient mGoogleApiClient;
+    Location location;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +61,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content, new ChampionListFragment())
+                .commit();
 
-        mapFragment.getMapAsync(this);
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
     }
 
 
@@ -53,14 +91,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_champions) {
 
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.content, new ChampionListFragment())
                     .commit();
 
-        } else if (id == R.id.nav_gallery) {
-
+        } else if (id == R.id.nav_maps) {
+            mapFragment.getMapAsync(this);
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.content, mapFragment)
                     .commit();
@@ -72,14 +110,116 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        map = mapFragment.getMap();
+        /*map.setMyLocationEnabled(true);
+        map.setTrafficEnabled(true);*/
+        map.setOnMapLongClickListener(this);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        criteria = new Criteria();
+        markerOptions = new MarkerOptions();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        if (location != null) {
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 13));
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                    .zoom(17)                   // Sets the zoom
+                    .bearing(90)                // Sets the orientation of the camera to east
+                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_directions_run_black_48dp));
+            markerOptions.title("Aca estoy");
+            markerOptions.position(new LatLng(location.getLatitude(), location.getLongitude()));
+            map.addMarker(markerOptions);
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Debe habilitar su ubicacion si desea utilizar esta seccion",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
     @Override
     public void onFragmentInteraction(Uri uri) {
 
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+    }
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location != null) {
+
+
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        //Mostrar fragmento con info
+        return false;
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        map.clear();
+        map.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_directions_run_black_48dp)).title("Nuevo marcador").snippet(latLng.toString()));
     }
 }
 
